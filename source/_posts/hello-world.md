fe474f0a27b9951bc277c2e11d776a89dbda256a
Mark S. Rasmussen <msr@ipaper.dk>
2014-02-13 21:57:08 +0100

    Initial commit

---
+++ source/_posts/hello-world.md
@@ -0,0 +1,6 @@
+title: Hello World
+date: 2014-02-13 15:13:59
+tags:
+---
+
+Welcome to [Hexo](http://zespia.tw/hexo)! This is your very first post. Check [documentation](http://zespia.tw/hexo/docs) to learn how to use.
---
+++ themes/landscape/Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		_clean: {
+			tmp: ['tmp']
+		}
+	});
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.renameTask('clean', '_clean');
+	grunt.registerTask('clean', ['_clean']);
+};
